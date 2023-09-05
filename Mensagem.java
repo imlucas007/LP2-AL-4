9b7448fffd7a3687d9aee091bd8ea3bff2996de9
Lucas4567 <135055872+Lucas4567@users.noreply.github.com>
2023-09-05 09:56:41 -0300

    Mensagem.java

---
+++ Mensagem.java
@@ -0,0 +1,24 @@
+package Mensagens;
+
+public class Mensagem {
+	
+	private String email;
+	
+	private String mensagem;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+}
